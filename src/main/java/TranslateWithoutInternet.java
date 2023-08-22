374dd6f7edce31c4fac4ae559882f97742321281
אודיה סעדון <osadoun@g.jct.ac.il>
2023-08-22 15:08:20 +0300

    fix in TranslateWithoutInternet

--- src/main/java/TranslateWithoutInternet.java
+++ src/main/java/TranslateWithoutInternet.java
@@ -1,15 +1,10 @@
 import java.sql.*;
 
-
 public class TranslateWithoutInternet {
     public static void main(String[] args) {
-
         //insertToDatabase("ב" , "b");
         //System.out.println(retrieveHebrewValues("בא"));
-
     }
-
-
 
     public static char[] breakWordIntoLetters(String word) {
         char[] letters = new char[word.length()];
@@ -21,29 +16,41 @@ public class TranslateWithoutInternet {
         return letters;
     }
 
-
     public static String retrieveHebrewValues(String word) {
-        String jdbcUrl = "jdbc:sqlserver://LOCALHOST\\SQLEXPRESS:1433;databaseName=logistcourse1;SelectMethod=Cursor";
-        String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        // Database credentials
-        String username = "logistcourse1";
-        String password = "logistcourse1";
+        // Define constants for the database connection information
+        final String JDBC_URL = "jdbc:sqlserver://LOCALHOST\\SQLEXPRESS:1433;databaseName=logistcourse1;SelectMethod=Cursor";
+        final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        final String USERNAME = "logistcourse1";
+        final String PASSWORD = "logistcourse1";
+
         // Database credentials
         Connection conn = null;
         Statement stmt = null;
+
         //Connect to the database
         ResultSet rs = null;
 
         char[] letters=breakWordIntoLetters(word);
         StringBuilder wordBuilder = new StringBuilder();
+
         try {
-            Class.forName(jdbcDriver);
-            conn = DriverManager.getConnection(jdbcUrl, username, password);
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
             stmt = conn.createStatement();
+
+            // Prepare the query
+            String selectQuery = "SELECT English FROM Copying WHERE Hebrew = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(selectQuery);
+
+            // Loop over the letters
             for (char letter : letters) {
-                String myLetter = String.valueOf(letter);
-                String selectQuery = "SELECT English FROM Copying WHERE Hebrew = '" + myLetter + "'";
-                rs = stmt.executeQuery(selectQuery);
+                // Set the parameter value
+                preparedStatement.setString(1, String.valueOf(letter));
+
+                // Execute the query
+                rs = preparedStatement.executeQuery();
+
+                // If the query returns a row, get the English value
                 if (rs.next()) {
                     String EnglishValue = rs.getString("English");
                     wordBuilder.append(EnglishValue);
@@ -70,7 +77,6 @@ public class TranslateWithoutInternet {
         return wordBuilder.toString();
     }
 
-
     public static void insertToDatabase(String hebrew, String english){
         String jdbcUrl = "jdbc:sqlserver://LOCALHOST\\SQLEXPRESS:1433;databaseName=logistcourse1;SelectMethod=Cursor";
         String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
@@ -85,12 +91,16 @@ public class TranslateWithoutInternet {
         try{
             Class.forName(jdbcDriver);
             conn = DriverManager.getConnection(jdbcUrl, username, password);
-            stmt = conn.createStatement();
-            String insertQuery = "INSERT INTO Copying (Hebrew, English) VALUES ('" + hebrew + "', '" + english + "')";
-            stmt.executeUpdate(insertQuery);
-            stmt.close();
-            conn.close();
+            String insertQuery = "INSERT INTO Copying (Hebrew, English) VALUES (?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(insertQuery);
 
+            preparedStatement.setString(1, hebrew);
+            preparedStatement.setString(2, english);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+            conn.close();
         }
         catch (ClassNotFoundException e) {
             e.printStackTrace();
