a3e75d80cfbfc677e293f9f7c512f06373a05f25
אודיה סעדון <osadoun@g.jct.ac.il>
2023-08-22 14:35:23 +0300

    rename file

--- src/main/java/TranslateWithoutInternet.java
+++ src/main/java/TranslateWithoutInternet.java
@@ -1,19 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.sql.*;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 
-public class Translate {
+public class TranslateWithoutInternet {
     public static void main(String[] args) {
 
         //insertToDatabase("ב" , "b");
