2f0dc1c9766c88dff8e86e1c1d8d805db5533afe
אודיה סעדון <osadoun@g.jct.ac.il>
2023-10-04 12:14:51 +0300

    a little change in the meaning of numeric number that need to check if all the string is number not if its contains number

--- src/main/java/ClassifySentenceWithoutInternet.java
+++ src/main/java/ClassifySentenceWithoutInternet.java
@@ -70,7 +70,7 @@ public class ClassifySentenceWithoutInternet {
                 continue;
             }
 
-            if(isNumericNumber(word, conn, stmt, rs) || GetType.getNumericWord(word)){
+            if(isNumericNumber(word, conn, stmt, rs) ){
                 continue;
             }
             if(i + 2 < lstTemplate.size()) { //תוספת על הענין של אופרטורים 2 מילים לא שמורות ובינהן אופרטור שתיהן שדות בנושא ראשי
@@ -427,28 +427,21 @@ public class ClassifySentenceWithoutInternet {
     }
 
     private static boolean isNumericNumber(String s, Connection conn, Statement stmt, ResultSet rs) {
+
+        // Check if the string is empty or null.
         if (s == null || s.isEmpty()) {
             return false;
         }
 
-        // Check if the string contains only digits.
-        boolean containsDigits = true;
+        // Check if the string contains any non-numeric characters.
         for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                return true;
+            if (!Character.isDigit(c)) {
+                return false;
             }
         }
 
-        // Check if the string is a Hebrew number or contains only digits.
-//        if (s.equals("אחת") || s.equals("שתיים") || s.equals("שלוש") || s.equals("ארבע") || s.equals("חמש")
-//                || s.equals("שש") || s.equals("שבע") || s.equals("שמונה") || s.equals("תשע") || s.equals("עשר")
-//                || s.equals("אחד") || s.equals("שניים") || s.equals("שלושה") || s.equals("ארבעה") || s.equals("חמישה")
-//                || s.equals("שישה") || s.equals("שיבעה") || s.equals("תשעה") || s.equals("עשרה")) {
-//            return true;
-//        }
-
-        // If the string is not empty and does not represent a numeric value, return false.
-        return false;
+        // If the string contains only digits, return true.
+        return true;
     }
 
     public static String changePluralSubjectToSingle(String pluralSubject) {
